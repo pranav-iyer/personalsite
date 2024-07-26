cf16f275063f2a7add63541619ff944fa71fad46
Pranav Iyer <piyer2b@gmail.com>
2024-07-26 13:01:14 +0200

    fix bug when dates are empty

--- personalsite/pranav_tracker/views.py
+++ personalsite/pranav_tracker/views.py
@@ -12,22 +12,26 @@ def index(request):
     if form.is_valid():
         start_date = form.cleaned_data["start_date"]
         end_date = form.cleaned_data["end_date"]
-
-        start_ts = datetime(start_date.year, start_date.month, start_date.day, 0, 0, 0)
-        end_ts = datetime(end_date.year, end_date.month, end_date.day, 23, 59, 59)
-        locations = list(
-            Location.objects.filter(timestamp__range=(start_ts, end_ts))
-            .order_by("timestamp")
-            .values(
-                "id",
-                "timestamp",
-                "latitude",
-                "longitude",
-                "position_accuracy",
-                "heading",
-                "speed",
+        if not start_date or not end_date:
+            locations = []
+        else:
+            start_ts = datetime(
+                start_date.year, start_date.month, start_date.day, 0, 0, 0
             )
-        )
+            end_ts = datetime(end_date.year, end_date.month, end_date.day, 23, 59, 59)
+            locations = list(
+                Location.objects.filter(timestamp__range=(start_ts, end_ts))
+                .order_by("timestamp")
+                .values(
+                    "id",
+                    "timestamp",
+                    "latitude",
+                    "longitude",
+                    "position_accuracy",
+                    "heading",
+                    "speed",
+                )
+            )
     else:
         locations = []
     print(locations)
