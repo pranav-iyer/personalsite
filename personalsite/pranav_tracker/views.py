5296c2fc097ed87b38fde02b490f757552623702
Pranav Iyer <piyer2b@gmail.com>
2024-07-27 10:12:43 +0200

    remove circles

--- personalsite/pranav_tracker/views.py
+++ personalsite/pranav_tracker/views.py
@@ -10,17 +10,17 @@ from .forms import IndexForm
 def index(request):
     form = IndexForm(request.GET)
     if form.is_valid():
-        start_date = form.cleaned_data["start_date"]
-        end_date = form.cleaned_data["end_date"]
-        if not start_date or not end_date:
+        view_date = form.cleaned_data["view_date"]
+        if not view_date:
             locations = []
         else:
-            start_ts = datetime(
-                start_date.year, start_date.month, start_date.day, 0, 0, 0
+            start_ts = datetime(view_date.year, view_date.month, view_date.day, 0, 0, 0)
+            end_ts = datetime(
+                view_date.year, view_date.month, view_date.day, 23, 59, 59
             )
-            end_ts = datetime(end_date.year, end_date.month, end_date.day, 23, 59, 59)
             locations = list(
                 Location.objects.filter(timestamp__range=(start_ts, end_ts))
+                .filter(position_accuracy__lte=300)
                 .order_by("timestamp")
                 .values(
                     "id",
@@ -34,7 +34,6 @@ def index(request):
             )
     else:
         locations = []
-    print(locations)
     return render(
         request, "pranav_tracker/index.html", {"form": form, "locations": locations}
     )
