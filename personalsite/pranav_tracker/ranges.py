442b2883967d397f8f32d6dcad07b86e3d6018ef
Pranav Iyer <piyer2b@gmail.com>
2026-02-17 20:11:59 +0100

    backend returns ranges of time through which position is stationary

---
+++ personalsite/pranav_tracker/ranges.py
@@ -0,0 +1,91 @@
+from dataclasses import dataclass
+from typing import Any, Dict, List
+import shapely
+import geopandas
+import pandas as pd
+import numpy as np
+
+
+def print_time(ts: pd.Timestamp) -> str:
+    return ts.tz_convert("Europe/Brussels").strftime("%Y-%m-%d %H:%M:%S")
+
+
+def get_centroid_latlon(gdf) -> shapely.Point:
+    centroid = shapely.Point(gdf.geometry.x.mean(), gdf.geometry.y.mean())
+    return geopandas.GeoSeries([centroid], crs=gdf.crs).to_crs("EPSG:4326")[0]
+
+
+@dataclass
+class Range:
+    start: shapely.Point
+    start_idx: int
+    end: shapely.Point
+    end_idx: int
+    centroid: shapely.Point
+
+    def __str__(self):
+        return repr(self)
+
+    def __repr__(self):
+        return f"Range [{self.start_idx}-{self.end_idx}] (<{print_time(self.start.timestamp)}> - <{print_time(self.end.timestamp)}>)"
+
+    def distance(self, other: "Range") -> float:
+        if self.start_idx < other.end_idx:
+            return other.distance(self)
+        else:
+            return self.start.geometry.distance(other.end.geometry)
+
+    def merge(self, other: "Range") -> "Range":
+        if self.start_idx < other.end_idx:
+            return other.merge(self)
+        else:
+            self.start = other.start
+            self.start_idx = other.start_idx
+            return self
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            "latitude": self.centroid.y,
+            "longitude": self.centroid.x,
+            "start_time": self.start.timestamp.tz_convert(
+                "Europe/Brussels"
+            ).isoformat(),
+            "end_time": self.end.timestamp.tz_convert("Europe/Brussels").isoformat(),
+        }
+
+
+def get_stationary_ranges(gdf) -> List[Range]:
+    THRESH = 2e1
+    d = np.vstack([gdf.geometry.x, gdf.geometry.y])
+    N = d.shape[1]
+    i = 0
+    min_ws = 5
+    ranges = []
+    while i < N - min_ws:
+        for j in range(i + min_ws, N):
+            if np.linalg.norm(np.std(d[:, i:j], axis=1)) > THRESH:
+                dt = gdf.iloc[j - 1].timestamp - gdf.iloc[i].timestamp
+                if dt > pd.Timedelta("7 minutes"):
+                    rowI = gdf.iloc[i]
+                    rowJ = gdf.iloc[j]
+                    ranges.append(
+                        Range(rowI, i, rowJ, j, get_centroid_latlon(gdf.iloc[i:j]))
+                    )
+                break
+        else:
+            dt = gdf.iloc[j - 1].timestamp - gdf.iloc[i].timestamp
+            if dt > pd.Timedelta("7 minutes"):
+                rowI = gdf.iloc[i]
+                rowJ = gdf.iloc[j]
+                ranges.append(
+                    Range(rowI, i, rowJ, j, get_centroid_latlon(gdf.iloc[i:j]))
+                )
+            break
+        i = j
+
+    # merge ranges where previous ends very close to next's start
+    for i in range(len(ranges) - 1, 1, -1):
+        if ranges[i].distance(ranges[i - 1]) < 50:
+            ranges[i - 1] = ranges[i].merge(ranges[i - 1])
+            ranges.pop(i)
+    return [r.to_dict() for r in ranges]
--- personalsite/pranav_tracker/views.py
+++ personalsite/pranav_tracker/views.py
@@ -1,5 +1,10 @@
 from datetime import datetime
 from django.shortcuts import render
+from django.http import JsonResponse
+import pandas as pd
+import geopandas
+
+from personalsite.pranav_tracker import ranges
 
 from .models import Location
 
@@ -13,27 +18,55 @@ def index(request):
         view_date = form.cleaned_data["view_date"]
         if not view_date:
             locations = []
+            ranges = []
         else:
-            start_ts = datetime(view_date.year, view_date.month, view_date.day, 0, 0, 0)
-            end_ts = datetime(
-                view_date.year, view_date.month, view_date.day, 23, 59, 59
-            )
-            locations = list(
-                Location.objects.filter(timestamp__range=(start_ts, end_ts))
-                .filter(position_accuracy__lte=300)
-                .order_by("timestamp")
-                .values(
-                    "id",
-                    "timestamp",
-                    "latitude",
-                    "longitude",
-                    "position_accuracy",
-                    "heading",
-                    "speed",
-                )
-            )
+            locations = get_locations(view_date)
+            ranges = get_ranges(locations)
     else:
         locations = []
+        ranges = []
     return render(
         request, "pranav_tracker/index.html", {"form": form, "locations": locations}
     )
+
+
+def get_locations(view_date: datetime):
+    start_ts = datetime(view_date.year, view_date.month, view_date.day, 0, 0, 0)
+    end_ts = datetime(view_date.year, view_date.month, view_date.day, 23, 59, 59)
+    return list(
+        Location.objects.filter(timestamp__range=(start_ts, end_ts))
+        .filter(position_accuracy__lte=300)
+        .order_by("timestamp")
+        .values(
+            "id",
+            "timestamp",
+            "latitude",
+            "longitude",
+            "position_accuracy",
+            "heading",
+            "speed",
+        )
+    )
+
+
+def ranges_view(request):
+    form = IndexForm(request.GET)
+    ranges = []
+    if form.is_valid():
+        view_date = form.cleaned_data["view_date"]
+        if view_date:
+            locations = get_locations(view_date)
+            ranges = get_ranges(locations)
+    return JsonResponse({"ranges": ranges})
+
+
+def get_ranges(locations: list):
+    df = pd.DataFrame(locations)
+    gdf = geopandas.GeoDataFrame(
+        df,
+        geometry=geopandas.points_from_xy(df.longitude, df.latitude),
+        crs="EPSG:4326",
+    )
+    utm_crs = gdf.estimate_utm_crs()
+    gdf = gdf.to_crs(utm_crs)
+    return ranges.get_stationary_ranges(gdf)
